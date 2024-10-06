a645aa001c534444d72b7195f5eff2e52bb22327
vijesh0512 <vijeshg219@gmail.com>
2024-10-06 22:55:03 +0530

    Update content.jsx

--- src/Components/content.jsx
+++ src/Components/content.jsx
@@ -317,7 +317,7 @@ const Heros = ({ onNavClick,onSongChange, onAudioChange }) => {
       renderSongs(singer, 'singer-player');
       renderSongs(Years, 'others-player');
       rendermov(movies, 'movie-player');
-      rendermov(stream, 'stream-player');
+      //rendermov(stream, 'stream-player');
       renderchannel(channels, 'live-player');
       renderdog(others, 'dog-player');
     }
@@ -350,10 +350,10 @@ const Heros = ({ onNavClick,onSongChange, onAudioChange }) => {
       ) : (
         <>
  
-          <div className="bt">
+                            {/* <div className="bt">
           <h1 className='sideheading'>Live Sports</h1>
           </div>
-        <div id='stream-player' className='player'></div> 
+        <div id='stream-player' className='player'></div> */}
 
           <div className="bt">
               <h1 className='sideheading'>Movies</h1>
