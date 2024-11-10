7539c7305e45a2fac4ebb5d4338530cd02b23b88
vijesh0512 <vijeshg219@gmail.com>
2024-11-10 18:59:07 +0530

    Update video.jsx

--- src/Components/video.jsx
+++ src/Components/video.jsx
@@ -26,6 +26,67 @@ const shows = [
       ] 
   },
 
+  {
+    name: "Vettaiyan",
+    image: 'https://upload.wikimedia.org/wikipedia/en/6/68/Vettaiyan_poster.jpg',
+    shows: [
+        {
+        name: "Manasilaayo",
+        img: "https://keralakaumudi.com/web-news/en/2024/09/NMAN0527509/image/thumb/manju-warrier-dance-rajin.1.2895901.webp",
+        link: "https://youtu.be/5WsUIeNAtbM"
+      },
+      {
+        name: "Hunter Vantar",
+        img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202409/hunter-vantaar-200457802-16x9_0.jpg?VersionId=Y2YT0bzH2Sdo5Lc_hA.MqEJPNVIZGAwU&size=690:388",
+        link: "https://youtu.be/lIwhaTfqwzc"
+      },
+      ] 
+  },
+
+  { 
+    name: 'RAYAN', 
+    image:'https://m.media-amazon.com/images/M/MV5BODJhYjcwZTktZWY1Yy00ODNlLTg1MDQtOTdmZDliNWIyMmFhXkEyXkFqcGc@._V1_.jpg', 
+    songs: [
+      {
+        name: "Adangatha Asuran",
+        img: "https://i.ytimg.com/vi/rsouMX_N56E/maxresdefault.jpg",
+        link: "https://youtu.be/gk6Jri1kGTs"
+      },
+      {
+          name: "Water Packet",
+          img: "https://tse3.mm.bing.net/th?id=OIP.YLDc6NKQ5q__7-ISeVWeAAHaEK&pid=Api&P=0&h=220",
+          link: "https://youtu.be/RgOEKdA2mlw"
+        },
+        {
+          name: "Oh Raya",
+          img: "https://m.media-amazon.com/images/M/MV5BODJhYjcwZTktZWY1Yy00ODNlLTg1MDQtOTdmZDliNWIyMmFhXkEyXkFqcGc@._V1_.jpg",
+          link: "https://youtu.be/3WAgyvw3RDc"
+        },
+    ] 
+  },
+
+  { 
+    name: 'Jailer', 
+    image:'https://upload.wikimedia.org/wikipedia/en/c/cb/Jailer_2023_Tamil_film_poster.jpg', 
+    songs: [
+      {
+        name: "Kavala",
+        img: "https://static.toiimg.com/thumb/msid-101548900,width-1280,resizemode-4/101548900.jpg",
+        link: "https://youtu.be/lM8h5Mm6ODo"
+      },
+      {
+        name: "Hukum",
+        img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202307/748680-hukum-three_four.jpg?VersionId=helyHPHWgAM4XV_cixrp4PqAnBiz21HM",
+        link: "https://youtu.be/gB2zKZxESTg"
+      },
+      {
+        name: "Rathamarey",
+        img: "https://media.dailythanthi.com/h-upload/2023/08/05/1430343-jailer-song-07.webp",
+        link: "https://youtu.be/nDJ8KgjlzYM"
+      },
+    ] 
+  },
+
 ];
 
 const Video = () => {
