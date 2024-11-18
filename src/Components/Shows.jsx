c15a3755f2263c405c4ac8c2f00932109b25f355
vijesh0512 <vijeshg219@gmail.com>
2024-11-18 14:57:12 +0530

    Update Shows.jsx

--- src/Components/Shows.jsx
+++ src/Components/Shows.jsx
@@ -45,10 +45,12 @@ const shows = [
 ];
 
 
+
 const Movies = () => {
   const [selectedAlbum, setSelectedAlbum] = useState(null);
   const [currentVideo, setCurrentVideo] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [thumbnail, setThumbnail] = useState(null);
   const playerRef = React.useRef(null);
 
   useEffect(() => {
@@ -131,6 +133,7 @@ const Movies = () => {
 
   const handleVideoClick = (video) => {
     setCurrentVideo(video.link); // Set the MP4 video source
+    setThumbnail(video.img); // Set the video thumbnail
   };
 
   return (
@@ -143,7 +146,11 @@ const Movies = () => {
             </div>
           )}
           <div className="video-wrapper">
-            <video className="video-player"></video>
+            {/* Show thumbnail while loading */}
+            <div className={`video-thumbnail ${loading ? 'visible' : 'hidden'}`}>
+              {thumbnail && <img src={thumbnail} alt="Loading..." />}
+            </div>
+            <video className="video-player" />
           </div>
         </div>
       )}
