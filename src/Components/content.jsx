9bc2f1d07ac7b4da004283a0b8aa91b3329c4f4b
vijesh0512 <vijeshg219@gmail.com>
2024-12-04 18:35:58 +0530

    Update content.jsx

--- src/Components/content.jsx
+++ src/Components/content.jsx
@@ -12,7 +12,7 @@ const stream = [
 //  {name:'BGT',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVac-YFl-228U50PAvULH_bMEK8YJaPDFFOGthz54-dT5Lalc700E0MxI&s=10',link:'https://allinonereborn.com/dd.m3u8?id=23989'},
   { name: 'BB 24*7', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSvdIFaCYAcm_7BPwVQWaW6TKxbpvkTMvWswImFgaTwacr7G87XesElbHH&s=10', link: 'https://live.tamiltool.online/livestream/index.m3u8' },
 //  {name:'U19 Asia Cup',image:'https://tsneh.vercel.app/bgl0lder5dtp',link:'https://cricsters.io/event/play.php?id=1000299100'},
-
+  {name:'Tennis Premium League',image:'https://tsneh.vercel.app/7joamfh1g18-',link:'https://tsneh.vercel.app/vvd4vklzdd8l'},
 //{ name:'T10',image:'https://tinyurl.com/524jcpbd',link:'https://fancode.cricsters.io/play.php?id=112612&adsfree'},
 
 //  {name:'ISL',image:'https://tinyurl.com/3xan68yf',link:'https://tinyurl.com/veezszbr'},
