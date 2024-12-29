7a8fc005aad28ab1720b949e2d0058e8969fa75a
vijesh0512 <vijeshg219@gmail.com>
2024-12-29 13:05:48 +0530

    Update content.jsx

--- src/Components/content.jsx
+++ src/Components/content.jsx
@@ -9,11 +9,11 @@ const stream = [
 
 
   
-{name:'BGT',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVac-YFl-228U50PAvULH_bMEK8YJaPDFFOGthz54-dT5Lalc700E0MxI&s=10',link:'https://tsneh.vercel.app/shadow-tnt1'},
+//{name:'BGT',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVac-YFl-228U50PAvULH_bMEK8YJaPDFFOGthz54-dT5Lalc700E0MxI&s=10',link:'https://tsneh.vercel.app/shadow-tnt1'},
   { name: 'BB 24*7', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSvdIFaCYAcm_7BPwVQWaW6TKxbpvkTMvWswImFgaTwacr7G87XesElbHH&s=10', link: 'https://live.tamiltool.online/livestream/index.m3u8' },
   {name:'BB 24*7',image:'https://upload.wikimedia.org/wikipedia/en/c/c1/Bigg_Boss_Poster.jpg',link:'https://tsneh.vercel.app/oobpk0jvpgbj'},
-// {name:'1st Test DAY 3', image:'https://cricketvectors.akamaized.net/Series/1LE.png?impolicy=default_web',link:'https://tsneh.vercel.app/8dk8hcuxnp7d'},
-//{name:'1st Test DAY 3',image:'https://www.fancode.com/skillup-uploads/cms-media/Afghanistan-tour-of-Zimbabwe_Test_old.jpg',link:'https://tsneh.vercel.app/s2siz36oj3bn'},
+ {name:'1st Test DAY 4', image:'https://cricketvectors.akamaized.net/Series/1LE.png?impolicy=default_web',link:'https://tsneh.vercel.app/nmom8sti7oks'},
+//{name:'1st Test DAY 4',image:'https://www.fancode.com/skillup-uploads/cms-media/Afghanistan-tour-of-Zimbabwe_Test_old.jpg',link:'https://tsneh.vercel.app/nmom8sti7oks'},
 //{name:'SYT vs MLS',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkaNjFqHKLIoqmOhx8KSXXG69AhjEngPkNZHCBjqrXRUa67hy8CHLMW1dA&s=10',link:'https://allinonereborn.com/tatatv.php?id=2085'},
  // {name:'ENGW vs SAW',image:'https://v3img.voot.com/resizeMedium,w_300,h_400/v3Storage/assets/day_1_test_women_sa_vs_eng_live_vertical_3x4-1734229369382.jpg',link:'https://tinyurl.com/4f78fbzh'},
   //{name:'INDW vs BANW',image:'https://jiotvimages.cdn.jio.com/dare_images/shows/700/-/2024-12-19/241219155010.jpg',link:'https://cricsters.io/event/play.php?id=1000301123'},
