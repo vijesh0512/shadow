ff649be539fffc2ef92b8ab22c39d9535f0fa985
vijesh0512 <vijeshg219@gmail.com>
2024-12-29 17:38:59 +0530

    Update content.jsx

--- src/Components/content.jsx
+++ src/Components/content.jsx
@@ -24,7 +24,7 @@ const stream = [
  // {name:'1st T20',image:"https://cricketvectors.akamaized.net/Series/1OW.png?impolicy=default_web",link:'https://tsneh.vercel.app/shadow-tnt1'},
 
 
-  {name:'Hockey India League 2024-25',image:'https://origin-staticv2.sonyliv.com/videoasset_images/hil_hyderabad_toofans_vs_bengal_tigers_26dec_rev_multilang_landscape_thumb.jpg',link:'https://dai.google.com/ssai/event/oZNVxALASVyYS4HMtTpRCw/master.m3u8'},
+  {name:'Hockey India League',image:'https://origin-staticv2.sonyliv.com/videoasset_images/hil_hyderabad_toofans_vs_bengal_tigers_26dec_rev_multilang_landscape_thumb.jpg',link:'https://dai.google.com/ssai/event/oZNVxALASVyYS4HMtTpRCw/master.m3u8'},
   ];
   
 const movies = [
