bb667d6cd184d13ea33d5f7a7dee432d01c1fde1
vijesh0512 <vijeshg219@gmail.com>
2025-01-02 07:44:25 +0530

    Update movies.jsx

--- src/Components/movies.jsx
+++ src/Components/movies.jsx
@@ -115,7 +115,7 @@ const shows = [
     name: 'Dubbed Movies',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbkn91GpNi88CicRs375878Z6hZWXOpU4gvg&s',shows:[
       {name:'Baroz',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrCNfQUAEl91jWxWO1DzCkv3f6lllt5f1Uog&usqp=CAU',link:'https://dls4.mrfooll.xyz/AgADzx3251623'},
      {name:'Mufasa',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6yJpaTrFzHuiFC7L2wTo9C6-kzlDRFqNGAn5PuBYx5qkpNBq805GWRbUE&s=10',link:'https://dls4.mrfooll.xyz/AgADsh3233774'},
-      {name:'Pushpa 2',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTheRlM_HeB2-5rRx8YV3zDdGWpelHHHX3XLbdsl62EUI5eszSjUKkHq61k&s=10',link:'https://s06.hotshare.link/Moviesda.Mobi_-_Pushpa_2_The_Rule_2024_HDRip_1080p_HD.mp4'},
+      {name:'Pushpa 2',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTheRlM_HeB2-5rRx8YV3zDdGWpelHHHX3XLbdsl62EUI5eszSjUKkHq61k&s=10',link:'https://pub-c112d6c4191e458db7adfadb97398a27.r2.dev/Pushpa_2.mp4'},
       {name:'Karataka Damanaka',image:'https://multicinema.in/assets1/2021MoviesPoster/Karataka_Damanaka.jpg',link:'https://pub-c112d6c4191e458db7adfadb97398a27.r2.dev/Karataka_Damanaka.mp4'},
       {name:'Bagheera',image:'https://multicinema.in/assets1/2021MoviesPoster/Bagheera.jpg',link:'https://pub-c112d6c4191e458db7adfadb97398a27.r2.dev/Bagheera.mp4'},
       {name:"ARM",image:"https://multicinema.in/assets1/2021MoviesPoster/ARM.jpg",link:"https://pub-c112d6c4191e458db7adfadb97398a27.r2.dev/ARM.mp4"},
